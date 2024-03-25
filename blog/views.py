3d5b23bfa321803191aa20e7957c51c8a18fbfbf
Diyashaji21 <diyashaji21@gmail.com>
2024-03-25 10:21:23 +0530

    Create Skeleton framework for LocalLibrary

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,14 @@
 from django.shortcuts import redirect, render, get_object_or_404
 from django.utils import timezone
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
-# # Create your views here.
-# def post_list(request, pk):
-#     Self.pk=pk
-#     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-#     return render(request, 'blog/post_list.html', {'posts': posts})
 
-# def post_detail(request, pk):
-#     Self.pk=pk
-#     post = get_object_or_404(Post, pk=pk)
-#     return render(request, 'blog/post_detail.html', {'post': post})
-
-# def post_detail(request, pk):
-#     post = get_object_or_404(Post, pk=pk)
-#     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
@@ -74,3 +62,43 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+def admin_comments(request):
+    comments = Comment.objects.all()
+    return render(request, 'blog/admin_comments.html', {'comments': comments})
+from django.shortcuts import render
+from .models import Comment
+
+def comment_status(request):
+    if request.user.is_superuser:  # Check if the user is an admin
+        feedback = Comment.objects.filter(approved_comment=True)
+    else:
+        user_email = request.user.email  # Assuming users are logged in and have an email field
+        feedback = Comment.objects.filter(author=user_email, approved_comment=True)
+    return render(request, 'comment_status.html', {'feedback': feedback})
